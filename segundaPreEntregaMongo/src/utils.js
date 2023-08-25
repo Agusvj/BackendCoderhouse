d3bfce543140127612ad8e60b177a94288c19b78
AgusVJ <agustinvillanuevajousset@gmail.com>
2023-08-25 15:54:02 -0300

    Agregado logger

--- segundaPreEntregaMongo/src/utils.js
+++ segundaPreEntregaMongo/src/utils.js
@@ -4,6 +4,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 import { connect } from "mongoose";
 import bcrypt from "bcrypt";
+import winston from "winston";
 
 export default __dirname;
 
@@ -24,3 +25,40 @@ export const createHash = (password) =>
 
 export const isValidPassword = (password, hashPassword) =>
   bcrypt.compareSync(password, hashPassword);
+
+const customLevelsOptions = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5,
+  },
+  colors: {
+    fatal: "red",
+    error: "cyan",
+    warning: "yellow",
+    info: "green",
+    http: "blue",
+    debug: "white",
+  },
+};
+
+export const logger = winston.createLogger({
+  levels: customLevelsOptions.levels,
+  transports: [
+    new winston.transports.Console({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.colorize({ colors: customLevelsOptions.colors }),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      filename: "./error.log",
+      level: "info",
+      format: winston.format.simple(),
+    }),
+  ],
+});
